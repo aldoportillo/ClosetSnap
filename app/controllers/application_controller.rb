353335c832465bfb86f959e3c226a879595c59d2
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2023-11-11 16:42:18 -0600

    Add @q in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception, prepend: true
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_search
 
+  def set_search
+    @q = Item.where(:user_id => current_user.id).ransack(params[:q])
+  end
 
   protected
 
